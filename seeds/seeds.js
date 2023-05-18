b30cde47dd1da2ddd8cb4e658472e53c4db275df
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-05-17 18:18:49 -0700

    got reveiws to show in insomnia

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,12 +1,17 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
-const { Reviews} = require('../models');
+const { Reviews } = require('../models');
 const userData = require('./userData.json');
 const ReveiwsData = require('./ReviewsData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  })
+
+  await Reviews.bulkCreate(ReveiwsData, {
     individualHooks: true,
     returning: true,
   })
