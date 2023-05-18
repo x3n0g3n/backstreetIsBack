a79930f9998a4b1b72ce331d753a90039cb61c89
griffinw@protonmail.com <griffinw@protonmail.com>
2023-05-17 20:26:31 -0400

    adjusted bcrypt

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const { User } = require('../../models');
 
 //get all user route to test api in insomina
@@ -13,16 +14,19 @@ router.get('/', async (req, res)  => {
 });
 
 // create a user
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-  const userData = await User.create(req.body);
-  req.session.save(()=> {
-    req.session.user_id = userData.id;
-    req.session.logged_in = true;
-
-    req.status(200).json(userData);
-  });
-  } catch (error) {
+    const newUser = req.body;
+    // hash the password from 'req.body' and save to newUser
+    newUser.password = await bcrypt.hash(req.body.password, 10);
+    // create the newUser with the hashed password and save to DB
+    //salted 10 times
+    const userData = await User.create(newUser);
+    res.status(200).json(userData);
+    //200 means OK
+  } catch (err) {
+    res.status(400).json(err);
+    //400 means bad request
   }
 });
 
@@ -33,12 +37,13 @@ router.post('/login', async (req, res) => {
 
 // find a way to add the jurrasic guy for failed log in attempts
 
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
+if (!userData) {
+  res.status(400).json({
+    message: 'Incorrect email or password, please try again',
+    image: 'https://static.wikia.nocookie.net/ytmnd-fads/images/f/f4/Ah_Ah_Ah_you_din%E2%80%99t_say_the_magic_word.gif/revision/latest/scale-to-width-down/180?cb=20230324114521',
+  });
+  return;
+}
 
     // Verify the posted password with the password stored in the database
     const validPassword = await userData.checkPassword(req.body.password);
@@ -96,8 +101,6 @@ console.log(userData)
     res.status(500).json(err);
   }
 });
-
-
 
 router.delete('/:id', (req, res, next) => {
   // delete one product by its `id` value
