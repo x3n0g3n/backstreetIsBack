52e88660f21ffa538d431b58fc81acd792b7d184
griffinw@protonmail.com <griffinw@protonmail.com>
2023-05-21 16:20:36 -0400

    fixed heroku connection and env.

--- Config/connection.js
+++ Config/connection.js
@@ -7,15 +7,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME='bucket_db',
-    process.env.DB_USER='root',
-    process.env.DB_PASSWORD='password',
-
-
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, 
     {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
-      port: 3306
+      port: process.env.DB_PORT
     }
   );
 }
